840228d8920656a07bef9859c21978addbdf44d1
Tancrède <dupontmartin112@gmail.com>
2023-07-02 12:34:52 +0200

    Fichier d'import corrigé et commenté : FINI

--- import.py
+++ import.py
@@ -7,27 +7,33 @@ import unicodedata
 open_quizz_db_data = (
     ("Animaux", "Les chats", "https://www.codeavecjonathan.com/res/mission/openquizzdb_50.json"),
     ("Arts", "Musée du Louvre", "https://www.codeavecjonathan.com/res/mission/openquizzdb_86.json"),
-    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
+    #### le lien ci-dessous était érroné 
+    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124737627/OpenQuizzDB_124/openquizzdb_124.json"),
     ("Cinéma", "Alien", "https://www.codeavecjonathan.com/res/mission/openquizzdb_241.json"),
     ("Cinéma", "Star wars", "https://www.codeavecjonathan.com/res/mission/openquizzdb_90.json"),
 )
 
 
 def strip_accents(s):
+    """The strip_accents function removes accents from characters in a string using Unicode normalization."""
     return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 
 def get_quizz_filename(categorie, titre, difficulte):
+    """The get_quizz_filename function generates a filename for the JSON file based on the quiz's category, 
+    title, and difficulty. It uses the strip_accents function to remove accents and replaces spaces with underscores."""
     return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
 
 
 def generate_json_file(categorie, titre, url):
+    """The generate_json_file function takes the quiz's category, title, and URL as parameters. It creates a dictionary
+     out_questionnaire_data to hold the quiz data and makes a GET request to the specified URL."""
     out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
     out_questions_data = []
     response = requests.get(url)
-    data = json.loads(response.text)
+    data = json.loads(response.text) # The JSON response is loaded into the data variable, and the quiz data is extracted from the "quizz" key.
     all_quizz = data["quizz"]["fr"]
-    for quizz_title, quizz_data in all_quizz.items():
+    for quizz_title, quizz_data in all_quizz.items(): # The code iterates over each quiz in the all_quizz dictionary. For each quiz, it generates a filename using the get_quizz_filename function and sets the quiz's difficulty level in out_questionnaire_data.
         out_filename = get_quizz_filename(categorie, titre, quizz_title)
         print(out_filename)
         out_questionnaire_data["difficulte"] = quizz_title
@@ -35,18 +41,21 @@ def generate_json_file(categorie, titre, url):
             question_dict = {}
             question_dict["titre"] = question["question"]
             question_dict["choix"] = []
-            for ch in question["propositions"]:
+            for ch in question["propositions"]: # It then iterates over each question in the quiz and creates a dictionary question_dict to store the question's title and choices. The choices are extracted from the question's "propositions" field, and a tuple is created for each choice, indicating whether it is the correct answer or not.
                 question_dict["choix"].append((ch, ch==question["réponse"]))
-            out_questions_data.append(question_dict)
-        out_questionnaire_data["questions"] = out_questions_data
-        out_json = json.dumps(out_questionnaire_data)
+            out_questions_data.append(question_dict) # The question_dict is added to the out_questions_data list. This list stores all the questions for the quiz.
+        out_questionnaire_data["questions"] = out_questions_data # After processing all the questions, the out_questions_data list is assigned to out_questionnaire_data["questions"].
+        out_json = json.dumps(out_questionnaire_data) # The out_questionnaire_data dictionary is converted to JSON using json.dumps.
 
+        # The JSON data is written to a file with the generated filename using open, write, and close operations.
         file = open(out_filename, "w")
         file.write(out_json)
         file.close()
         print("end")
 
 
+# Finally, the code loops through the open_quizz_db_data tuple, which contains the quiz categories, titles, and URLs. 
+# It calls the generate_json_file function for each quiz, passing the corresponding data.
 for quizz_data in open_quizz_db_data:
     generate_json_file(quizz_data[0], quizz_data[1], quizz_data[2])
 
